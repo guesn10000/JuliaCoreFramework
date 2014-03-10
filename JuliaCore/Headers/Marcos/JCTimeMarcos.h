da5a2424ad332f8065c7a25fb23de04f166c129f
Jymn_Chen <guesn10000@126.com>
2014-03-10 23:59:00 +0800

    Initial version

---
+++ JuliaCore/Headers/Marcos/JCTimeMarcos.h
@@ -0,0 +1,17 @@
+//
+//  JCTimeMarcos.h
+//  JuliaCoreFramework
+//
+//  Created by Jymn_Chen on 14-3-10.
+//  Copyright (c) 2014年 Jymn_Chen. All rights reserved.
+//
+
+#ifndef JuliaCoreFramework_JCTimeMarcos_h
+#define JuliaCoreFramework_JCTimeMarcos_h
+
+#define __WEEK_TIMEINTERVAL__   7.0  * DAY_TIMEINTERVAL
+#define __DAY_TIMEINTERVAL__    24.0 * HOUR_TIMEINTERVAL
+#define __HOUR_TIMEINTERVAL__   60.0 * MINUTE_TIMEINTERVAL
+#define __MINUTE_TIMEINTERVAL__ 60.0
+
+#endif
